a42e1f306c5d068e73f90b1c9b3a18e33f66f9f4
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-12-24 22:54:08 +0100

    Skeleton of simplest command line program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate getopts;
+
+
+use getopts::Options;
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+    let argv: Vec<String> = env::args().collect();
+    let program = argv[0].clone();
+
+    let mut options = Options::new();
+    options.optflag("h", "help", "Show this usage message");
+
+    let args = options.parse(&argv[1..]).unwrap();
+    if args.opt_present("h") {
+        print_usage(&program, options);
+        return;
+    }
+}
+
+
+/// Print the instructions about invoking the program from the command line.
+fn print_usage(program: &str, opts: Options) {
+    println!("{}", opts.usage(&format!("Usage: {} [options]", program)));
 }
