6a3da104fb2ab2c2ba3141d71769ca57a07bab23
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-12-24 23:51:34 +0100

    MVP that supports only identity transform

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate getopts;
 
 use getopts::Options;
 use std::env;
+use std::io::{self, Read, Write, BufRead, BufReader, BufWriter};
 
 
 fn main() {
@@ -17,10 +18,32 @@ fn main() {
         print_usage(&program, options);
         return;
     }
+
+    let expr = args.free.first().map(|s| &**s).unwrap_or("");
+    apply(expr, io::stdin(), io::stdout());
 }
 
 
 /// Print the instructions about invoking the program from the command line.
 fn print_usage(program: &str, opts: Options) {
     println!("{}", opts.usage(&format!("Usage: {} [options]", program)));
+}
+
+
+/// Apply the expression to given input stream,
+/// writing to the given output stream.
+fn apply<R: Read, W: Write>(expr: &str, input: R, output: W) {
+    if !(expr == "" || expr == "_" || expr == "id") {
+        // TODO(xion): be less useless
+        panic!("NYI");
+    }
+
+    let reader = BufReader::new(input);
+    let mut writer = BufWriter::new(output);
+    for line in reader.lines() {
+        // TODO(xion): handle read errors
+        let line = line.unwrap();
+        // TODO(xion): handle write errors
+        write!(writer, "{}\n", line).unwrap();
+    }
 }
