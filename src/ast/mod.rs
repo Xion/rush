06403ba05d5bc6a498b56bb8a7b588f9716c1b42
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-01-01 12:26:06 +0100

    Extract AtomNode to a separate module

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,57 +1,12 @@
 //! Data structures representing the abstract syntax tree (AST)
 //! of parsed expressions.
 
+mod atom;
 mod binaryop;
 mod functioncall;
 mod unaryop;
 
+pub use self::atom::*;
 pub use self::binaryop::*;
 pub use self::functioncall::*;
 pub use self::unaryop::*;
-
-
-use std::str::FromStr;
-
-use eval::{Eval, EvalResult, Context, Value};
-
-
-pub struct AtomNode {
-    pub value: Value,
-}
-
-impl FromStr for AtomNode {
-    type Err = <Value as FromStr>::Err;
-
-    fn from_str(s: &str) -> Result<AtomNode, Self::Err> {
-        s.parse::<Value>().map(|v| AtomNode{value: v})
-    }
-}
-
-impl Eval for AtomNode {
-    fn eval(&self, context: &Context) -> EvalResult {
-        Ok(self.resolve(&context))
-    }
-}
-
-impl AtomNode {
-    /// Resolve a possible variable reference against given context.
-    ///
-    /// Returns the variable's Value (which may be just variable name as string),
-    /// or a copy of the original Value if it wasn't a reference.
-    fn resolve(&self, context: &Context) -> Value {
-        let mut result = self.value.clone();
-
-        // follow the chain of references until it bottoms out
-        loop {
-            match result {
-                Value::Symbol(sym) => {
-                    result = context.get_var(&sym)
-                        .map(Value::clone)
-                        .unwrap_or_else(move || Value::String(sym));
-                }
-                _ => { break; }
-            }
-        }
-        result
-    }
-}
