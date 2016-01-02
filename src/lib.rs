9628c957ac2351165290358eeeed3a43c2476537
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-01-02 12:51:47 +0100

    Extract a lib crate for testing

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+//! Root module for actual application logic.
+
+// NOTE: `nom` has to be declared before `log` because both define an error!
+// macro, and we want to use the one from `log`.
+#[macro_use]
+extern crate nom;
+#[macro_use]
+extern crate log;
+
+mod ast;
+mod eval;
+mod parse;
+
+
+use std::io::{self, Read, Write, BufRead, BufReader, BufWriter};
+
+use self::eval::{Eval, Context, Value};
+use self::parse::parse;
+
+
+/// Apply the expression to given input stream,
+/// writing to the given output stream.
+pub fn apply<R: Read, W: Write>(expr: &str, input: R, output: W) -> Result<(), io::Error> {
+    debug!("Using expression: {}", expr);
+
+    let ast = try!(parse(expr)
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e)));
+
+    let reader = BufReader::new(input);
+    let mut writer = BufWriter::new(output);
+    let mut context = Context::new();
+
+    let mut count = 0;
+    for line in reader.lines() {
+        let line = try!(line);
+        context.set_var("_",
+            line.parse::<Value>().unwrap_or_else(|_| Value::String(line)));
+
+        let result = try!(ast.eval(&context)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e)));
+        try!(write!(writer, "{}\n", result));
+
+        count += 1;
+    }
+
+    info!("Processed {} line(s) of input", count);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
-extern crate getopts;
+//! Module with the entry point of the binary.
 
-// NOTE: `nom` has to be declared before `log` because both define an error!
-// macro, and we want to use the one from `log`.
-#[macro_use]
-extern crate nom;
+extern crate getopts;
 #[macro_use]
 extern crate log;
 
-mod ast;
-mod eval;
+extern crate ap;
+
+
 mod logging;
-mod parse;
 
 
 use std::env;
-use std::io::{self, Read, Write, BufRead, BufReader, BufWriter};
+use std::io::{self, Write};
 use std::process::exit;
 
 use getopts::Options;
-
-use self::eval::{Eval, Context, Value};
-use self::parse::parse;
 
 
 fn main() {
@@ -39,7 +33,7 @@ fn main() {
     }
 
     let expr = args.free.join(" ");
-    if let Err(error) = apply(&expr, io::stdin(), io::stdout()) {
+    if let Err(error) = ap::apply(&expr, io::stdin(), io::stdout()) {
         error!("{:?}", error);
         exit(1);
     }
@@ -49,35 +43,5 @@ fn main() {
 /// Print the instructions about invoking the program from the command line.
 fn print_usage(program: &str, opts: Options) {
     writeln!(&mut io::stderr(), "{}",
-             opts.usage(&format!("Usage: {} [options]", program)));
-}
-
-
-/// Apply the expression to given input stream,
-/// writing to the given output stream.
-fn apply<R: Read, W: Write>(expr: &str, input: R, output: W) -> Result<(), io::Error> {
-    debug!("Using expression: {}", expr);
-
-    let ast = try!(parse(expr)
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e)));
-
-    let reader = BufReader::new(input);
-    let mut writer = BufWriter::new(output);
-    let mut context = Context::new();
-
-    let mut count = 0;
-    for line in reader.lines() {
-        let line = try!(line);
-        context.set_var("_",
-            line.parse::<Value>().unwrap_or_else(|_| Value::String(line)));
-
-        let result = try!(ast.eval(&context)
-            .map_err(|e| io::Error::new(io::ErrorKind::Other, e)));
-        try!(write!(writer, "{}\n", result));
-
-        count += 1;
-    }
-
-    info!("Processed {} line(s) of input", count);
-    Ok(())
+             opts.usage(&format!("Usage: {} [options]", program))).unwrap();
 }
