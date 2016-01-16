8583e50bfa0ba755998c68e69936755cf759c961
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-01-15 23:55:23 -0800

    Remove some boilerplate from function calls

--- src/eval/context.rs
+++ src/eval/context.rs
@@ -59,36 +59,34 @@ impl Functions {
     pub fn new() -> Functions {
         let mut fs = Functions{funcs: HashMap::new()};
 
-        fs.define_nullary("rand", Box::new(|| {
-            Some(Value::Float(rand::random::<f64>()))
-        }));
+        fs.define_nullary("rand", || Some(Value::Float(rand::random::<f64>())));
 
-        fs.define_unary("rev", Box::new(|value| {
+        fs.define_unary("rev", |value| {
             value.map_str(|s: &str| {
                 // TODO(xion): since this reverses chars not graphemes,
                 // it mangles some non-Latin strings;
                 // fix with unicode-segmentation crate
                 s.chars().rev().collect::<String>()
             })
-        }));
-        fs.define_unary("abs", Box::new(|value| {
+        });
+        fs.define_unary("abs", |value| {
             match value {
                 v @ Value::Integer(_) => v.map_int(i64::abs),
                 v @ Value::Float(_) => v.map_float(f64::abs),
                 _ => None,
             }
-        }));
-        fs.define_unary("len", Box::new(|value| {
+        });
+        fs.define_unary("len", |value| {
             match value {
                 Value::String(ref s) => Some(Value::Integer(s.len() as i64)),
                 _ => None,
             }
-        }));
-        fs.define_unary("str", Box::new(|value| value.to_string_value()));
-        fs.define_unary("int", Box::new(|value| value.to_int_value()));
-        fs.define_unary("float", Box::new(|value| value.to_float_value()));
+        });
+        fs.define_unary("str", |value| value.to_string_value());
+        fs.define_unary("int", |value| value.to_int_value());
+        fs.define_unary("float", |value| value.to_float_value());
 
-        fs.define_binary("at", Box::new(|idx, value| {
+        fs.define_binary("at", |idx, value| {
             match (idx, value) {
                 (Value::Integer(i),
                  Value::String(s)) => s.chars().nth(i as usize).map(|c| {
@@ -98,7 +96,7 @@ impl Functions {
                 }),
                 _ => None,
             }
-        }));
+        });
 
         return fs;
     }
@@ -107,8 +105,9 @@ impl Functions {
         self.funcs.get(&name.to_string()).map(|func| func(args))
     }
 
-    fn define(&mut self, name: &str,
-              func: Box<Fn(Args) -> Option<Value>>) -> &mut Self {
+    fn define<F>(&mut self, name: &str, func: F) -> &mut Self
+        where F: Fn(Args) -> Option<Value> + 'static
+    {
         let name = name.to_string();
         self.funcs.insert(name.to_owned(), Box::new(move |args: Args| {
             // TODO(xion): better error messages for different problems;
@@ -119,37 +118,40 @@ impl Functions {
         self
     }
 
-    fn define_nullary(&mut self, name: &str,
-                      func: Box<Fn() -> Option<Value>>) -> &mut Self {
-        self.define(name, Box::new(move |args: Args| {
+    fn define_nullary<F>(&mut self, name: &str, func: F) -> &mut Self
+        where F: Fn() -> Option<Value> + 'static
+    {
+        self.define(name, move |args: Args| {
             if args.len() == 0 {
                 func()
             } else {
                 None
             }
-        }))
+        })
     }
 
-    fn define_unary(&mut self, name: &str,
-                    func: Box<Fn(Value) -> Option<Value>>) -> &mut Self {
-        self.define(name, Box::new(move |args: Args| {
+    fn define_unary<F>(&mut self, name: &str, func: F) -> &mut Self
+        where F: Fn(Value) -> Option<Value> + 'static
+    {
+        self.define(name, move |args: Args| {
             if args.len() == 1 {
                 func(args[0].clone())
             } else {
                 None
             }
-        }))
+        })
     }
 
-    fn define_binary(&mut self, name: &str,
-                     func: Box<Fn(Value, Value) -> Option<Value>>) -> &mut Self {
-        self.define(name, Box::new(move |args: Args| {
+    fn define_binary<F>(&mut self, name: &str, func: F) -> &mut Self
+        where F: Fn(Value, Value) -> Option<Value> + 'static
+    {
+        self.define(name, move |args: Args| {
             if args.len() == 2 {
                 func(args[0].clone(), args[1].clone())
             } else {
                 None
             }
-        }))
+        })
     }
 }
 
