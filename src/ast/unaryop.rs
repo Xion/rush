eb1e2e855b53abab626ddf115babc3145a14551a
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-01-29 20:03:33 -0800

    Drop the unused eval! macro from UnaryOpNode

--- src/ast/unaryop.rs
+++ src/ast/unaryop.rs
@@ -23,9 +23,9 @@ impl Eval for UnaryOpNode {
     fn eval(&self, context: &Context) -> eval::Result {
         let arg = try!(self.arg.eval(&context));
         match &self.op[..] {
-            "+" => UnaryOpNode::eval_plus(&arg),
-            "-" => UnaryOpNode::eval_minus(&arg),
-            "!" => UnaryOpNode::eval_bang(&arg),
+            "+" => UnaryOpNode::eval_plus(arg),
+            "-" => UnaryOpNode::eval_minus(arg),
+            "!" => UnaryOpNode::eval_bang(arg),
             _ => Err(eval::Error::new(
                 &format!("unknown unary operator: `{}`", self.op)
             ))
@@ -34,52 +34,24 @@ impl Eval for UnaryOpNode {
 }
 
 
-/// Helper macro for defining how unary operators evaluate
-/// for different value types.
-///
-/// See the usage in UnaryOpNode.eval_X methods below.
-macro_rules! eval {
-    // (arg: &Foo) -> Bar { foo(arg) }
-    (($x:ident: &$t:ident) -> $rt:ident { $e:expr }) => {
-        if let &Value::$t(ref $x) = $x {
-            return Ok(Value::$rt($e));
-        }
-    };
-    // (arg: Foo) -> Bar { foo(arg) }
-    (($x:ident: $t:ident) -> $rt:ident { $e:expr }) => {
-        if let Value::$t($x) = *$x {
-            return Ok(Value::$rt($e));
-        }
-    };
-
-    // arg : &Foo { foo(arg) }
-    ($x:ident : &$t:ident { $e:expr }) => {
-        eval!(($x: &$t) -> $t { $e });
-    };
-    // arg : Foo { foo(arg) }
-    ($x:ident : $t:ident { $e:expr }) => {
-        eval!(($x: $t) -> $t { $e });
-    };
-}
-
 impl UnaryOpNode {
     /// Evaluate the "+" operator for one value.
-    fn eval_plus(arg: &Value) -> eval::Result {
-        eval!(arg : Integer { arg });
-        eval!(arg : Float { arg });
+    fn eval_plus(arg: Value) -> eval::Result {
+        eval1!(arg : Integer { arg });
+        eval1!(arg : Float { arg });
         UnaryOpNode::err("+", &arg)
     }
 
     /// Evaluate the "-" operator for one value.
-    fn eval_minus(arg: &Value) -> eval::Result {
-        eval!(arg : Integer { -arg });
-        eval!(arg : Float { -arg });
+    fn eval_minus(arg: Value) -> eval::Result {
+        eval1!(arg : Integer { -arg });
+        eval1!(arg : Float { -arg });
         UnaryOpNode::err("-", &arg)
     }
 
     /// Evaluate the "!" operator for one value.
-    fn eval_bang(arg: &Value) -> eval::Result {
-        eval!(arg : Boolean { !arg });
+    fn eval_bang(arg: Value) -> eval::Result {
+        eval1!(arg : Boolean { !arg });
         UnaryOpNode::err("!", &arg)
     }
 
