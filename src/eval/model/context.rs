457d77a05d38e2bc6c1abfe5f31bf4b57f955962
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-02-14 23:35:41 -0800

    Introduce context hierarchy

--- src/eval/model/context.rs
+++ src/eval/model/context.rs
@@ -10,20 +10,27 @@ use super::variables::Variables;
 ///
 /// Contains all the variable and function bindings that are used
 /// when evaluating an expression.
-pub struct Context {
+pub struct Context<'a> {
+    parent: Option<&'a Context<'a>>,
     vars: Variables,
     funcs: Functions,
 }
 
-impl Context {
-    pub fn new() -> Context {
-        Context{vars: Variables::new(), funcs: Functions::new()}
+impl<'a> Context<'a> {
+    pub fn new() -> Context<'a> {
+        Context{parent: None, vars: Variables::new(), funcs: Functions::new()}
+    }
+    pub fn with_parent(parent: &'a Context<'a>) -> Context<'a> {
+        Context{
+            parent: Some(parent), vars: Variables::new(), funcs: Functions::new()
+        }
     }
 
     #[allow(dead_code)]
     /// Retrieves the value for a variable if it exists.
     pub fn get_var(&self, name: &str) -> Option<&Value> {
         self.vars.get(&name.to_string())
+            .or_else(|| self.parent.and_then(|ctx| ctx.get_var(name)))
     }
 
     /// Set a value for a variable.
@@ -35,12 +42,18 @@ impl Context {
     /// Resolve given value, producing either it's copy
     /// or a value of a variable it's referring to.
     pub fn resolve(&self, value: &Value) -> Value {
-        self.vars.resolve(value)
+        let value = self.vars.resolve(value);
+        match self.parent {
+            Some(ctx) => ctx.resolve(&value),
+            _ => value,
+        }
     }
 
     /// Call a function of given name with given arguments.
     /// Returns Some(result), or None if the function couldn't be found.
     pub fn call_func(&self, name: &str, args: Args) -> eval::Result {
+        // TODO(xion): if function can't be found in current context,
+        // try the parent (merge var & func namespaces first, though)
         self.funcs.call(name, args)
     }
 }
