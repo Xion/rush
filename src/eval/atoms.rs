66f26688eb577e64bb8d04308d136263de4d4784
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-03-08 22:30:29 -0800

    Shorten ArrayNode.eval

--- src/eval/atoms.rs
+++ src/eval/atoms.rs
@@ -2,7 +2,7 @@
 //! i.e. those that create the values that are then operated upon.
 
 use eval::{self, Context, Eval, Value};
-use eval::model::value::ObjectRepr;
+use eval::model::value::{ArrayRepr, ObjectRepr};
 use parse::ast::{ArrayNode, ObjectNode, ScalarNode};
 
 
@@ -17,17 +17,11 @@ impl Eval for ScalarNode {
 /// Evaluate the AST node representing an array value.
 impl Eval for ArrayNode {
     fn eval(&self, context: &Context) -> eval::Result {
-        // evaluate all the elements first and bail if any of that fails
-        // TODO(xion): like the evaluation of objects, make this lazy
-        // (currently, all items are evaluated, even if others already failed)
-        let evals: Vec<_> =
-            self.elements.iter().map(|x| x.eval(&context)).collect();
-        if let Some(res) = evals.iter().find(|r| r.is_err()) {
-            return res.clone();
+        let mut elems = ArrayRepr::new();
+        for ref x in self.elements.iter() {
+            let elem = try!(x.eval(&context));
+            elems.push(elem);
         }
-
-        // extract the element values and create the array
-        let elems = evals.into_iter().map(|r| r.ok().unwrap()).collect();
         Ok(Value::Array(elems))
     }
 }
