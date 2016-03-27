820361f38643dd3700e20d7acfebbc043a0cebe7
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-03-27 15:45:24 -0700

    Extract evaluation of conditional operator to separate module

---
+++ src/eval/operators/conditional.rs
@@ -0,0 +1,20 @@
+//! Module implementating evaluation of conditonal operator AST nodes.
+
+use eval::{self, api, Context, Eval};
+use parse::ast::ConditionalNode;
+
+
+/// Evaluate the ternary operator / conditional node.
+impl Eval for ConditionalNode {
+    #[inline]
+    fn eval(&self, context: &Context) -> eval::Result {
+        let condition = try!(
+            self.cond.eval(&context).and_then(api::conv::bool)
+        ).unwrap_bool();
+        if condition {
+            self.then.eval(&context)
+        } else {
+            self.else_.eval(&context)
+        }
+    }
+}
--- src/eval/operators/mod.rs
+++ src/eval/operators/mod.rs
@@ -1,28 +1,6 @@
 //! Module implementing evaluation of operator-related AST nodes.
-//!
-//! This includes unary and conditional operators, as well as binary operators
-//! and their curried versions (which evaluate into functions).
 
 mod unary;
 mod binary;
 mod curried;
-
-
-use eval::{self, api, Context, Eval, Value};
-use parse::ast::{ConditionalNode};
-
-
-/// Evaluate the ternary operator / conditional node.
-impl Eval for ConditionalNode {
-    #[inline]
-    fn eval(&self, context: &Context) -> eval::Result {
-        let condition = try!(
-            self.cond.eval(&context).and_then(api::conv::bool)
-        ).unwrap_bool();
-        if condition {
-            self.then.eval(&context)
-        } else {
-            self.else_.eval(&context)
-        }
-    }
-}
+mod conditional;
