5b6480d29e4f37a0ffd276e18deaaaf1eac535a9
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-03-27 15:43:09 -0700

    Extract evaluation of curried binary operators to separate module

---
+++ src/eval/operators/curried.rs
@@ -0,0 +1,72 @@
+//! Module implementing evaluation of curried binary operator AST nodes.
+
+use eval::{self, Eval, Context, Value};
+use eval::model::function::{Args, Arity, Function};
+use parse::ast::{BinaryOpNode, CurriedBinaryOpNode};
+
+
+impl Eval for CurriedBinaryOpNode {
+    fn eval(&self, context: &Context) -> eval::Result {
+        if let Some(ref left) = self.left {
+            let arg = try!(left.eval(&context));
+            return self.eval_with_left(arg);
+        }
+        if let Some(ref right) = self.right {
+            let arg = try!(right.eval(&context));
+            return self.eval_with_right(arg);
+        }
+        self.eval_with_none()
+    }
+}
+
+impl CurriedBinaryOpNode {
+    fn eval_with_left(&self, arg: Value) -> eval::Result {
+        let op = self.op.clone();
+        let func = move |args: Args, ctx: &Context| {
+            let other = try!(take_one_arg(args));
+            BinaryOpNode::eval_op(&op, arg.clone(), other, &ctx)
+        };
+        Ok(Value::Function(Function::from_native_ctx(Arity::Exact(1), func)))
+    }
+
+    fn eval_with_right(&self, arg: Value) -> eval::Result {
+        let op = self.op.clone();
+        let func = move |args: Args, ctx: &Context| {
+            let other = try!(take_one_arg(args));
+            BinaryOpNode::eval_op(&op, other, arg.clone(), &ctx)
+        };
+        Ok(Value::Function(Function::from_native_ctx(Arity::Exact(1), func)))
+    }
+
+    fn eval_with_none(&self) -> eval::Result {
+        let op = self.op.clone();
+        let func = move |args: Args, ctx: &Context| {
+            let (left, right) = try!(take_two_args(args));
+            BinaryOpNode::eval_op(&op, left, right, &ctx)
+        };
+        Ok(Value::Function(Function::from_native_ctx(Arity::Exact(2), func)))
+    }
+}
+
+
+// Utility functions
+
+fn take_one_arg(args: Args) -> eval::Result {
+    try!(ensure_argcount(&args, 1));
+    Ok(args.into_iter().next().unwrap())
+}
+fn take_two_args(args: Args) -> Result<(Value, Value), eval::Error> {
+    try!(ensure_argcount(&args, 2));
+    let mut args = args.into_iter();
+    Ok((args.next().unwrap(), args.next().unwrap()))
+}
+
+fn ensure_argcount(args: &Args, count: usize) -> Result<(), eval::Error> {
+    if args.len() != count {
+        return Err(eval::Error::new(&format!(
+            "invalid number of arguments: expected {}, got {}",
+            count, args.len()
+        )));
+    }
+    Ok(())
+}
--- src/eval/operators/mod.rs
+++ src/eval/operators/mod.rs
@@ -5,71 +5,11 @@
 
 mod unary;
 mod binary;
+mod curried;
 
 
 use eval::{self, api, Context, Eval, Value};
-use eval::model::function::{Args, Arity, Function};
-use parse::ast::{BinaryOpNode, ConditionalNode, CurriedBinaryOpNode};
-
-
-/// Evaluate the curried binary operator node.
-impl Eval for CurriedBinaryOpNode {
-    fn eval(&self, context: &Context) -> eval::Result {
-        if let Some(ref left) = self.left {
-            let arg = try!(left.eval(&context));
-            return self.eval_with_left(arg);
-        }
-        if let Some(ref right) = self.right {
-            let arg = try!(right.eval(&context));
-            return self.eval_with_right(arg);
-        }
-        self.eval_with_none()
-    }
-}
-impl CurriedBinaryOpNode {
-    fn eval_with_left(&self, arg: Value) -> eval::Result {
-        let op = self.op.clone();
-        let func = move |args: Args, ctx: &Context| {
-            let other = try!(CurriedBinaryOpNode::take_one_arg(args));
-            BinaryOpNode::eval_op(&op, arg.clone(), other, &ctx)
-        };
-        Ok(Value::Function(Function::from_native_ctx(Arity::Exact(1), func)))
-    }
-    fn eval_with_right(&self, arg: Value) -> eval::Result {
-        let op = self.op.clone();
-        let func = move |args: Args, ctx: &Context| {
-            let other = try!(CurriedBinaryOpNode::take_one_arg(args));
-            BinaryOpNode::eval_op(&op, other, arg.clone(), &ctx)
-        };
-        Ok(Value::Function(Function::from_native_ctx(Arity::Exact(1), func)))
-    }
-
-    fn eval_with_none(&self) -> eval::Result {
-        let op = self.op.clone();
-        let func = move |args: Args, ctx: &Context| {
-            if args.len() != 2 {
-                return Err(eval::Error::new(&format!(
-                    "invalid number of arguments: expected {}, got {}",
-                    2, args.len()
-                )));
-            }
-            let mut args = args.into_iter();
-            BinaryOpNode::eval_op(&op, args.next().unwrap(), args.next().unwrap(), &ctx)
-        };
-        Ok(Value::Function(Function::from_native_ctx(Arity::Exact(2), func)))
-    }
-
-    fn take_one_arg(args: Args) -> eval::Result {
-        if args.len() != 1 {
-            Err(eval::Error::new(&format!(
-                "invalid number of arguments: expected {}, got {}",
-                1, args.len()
-            )))
-        } else {
-            Ok(args.into_iter().next().unwrap())
-        }
-    }
-}
+use parse::ast::{ConditionalNode};
 
 
 /// Evaluate the ternary operator / conditional node.
