5a31f1c53790c000b2741ff23836bd1b0747744e
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-05-08 18:02:11 -0700

    Clean up the API-defining Context methods a little

--- crates/librush/src/eval/model/context/defines.rs
+++ crates/librush/src/eval/model/context/defines.rs
@@ -13,9 +13,16 @@ use eval::model::{Args, Arity, Function, Name};
 use eval::{self, Context, Error, Value};
 
 
-// Methods for defining the "pure" API functions
-// (those that don't access the Context directly).
+// Base methods for defining functions with any arity.
 impl<'c> Context<'c> {
+    /// Define a regular function with given arity, implemented using native Rust function.
+    ///
+    /// Functions defined this way may only use their arguments (and arbirary Rust code,
+    /// of course) to compute their result. One of their limitations is that they cannot
+    /// indirectly invoke other functions (i.e. those that are only represented as Value objects).
+    ///
+    /// Returns a reference to the Context for easy chaining.
+    ///
     pub fn define<'n, N: ?Sized, F>(&mut self, name: &'static N, arity: Arity, func: F) -> &mut Self
         where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
               F: Fn(Args) -> eval::Result + 'static
@@ -31,73 +38,14 @@ impl<'c> Context<'c> {
         self
     }
 
-    pub fn define_nullary<N:? Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
-        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
-              F: Fn() -> eval::Result + 'static
-    {
-        self.define(name, Arity::Exact(0), move |_| { func() })
-    }
-    pub fn define_nullary_plus<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
-        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
-              F: Fn(Args) -> eval::Result + 'static
-    {
-        self.define(name, Arity::Minimum(0), func)
-    }
-
-    pub fn define_unary<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
-        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
-              F: Fn(Value) -> eval::Result + 'static
-    {
-        self.define(name, Arity::Exact(1), move |args: Args| {
-            let mut args = args.into_iter();
-            func(args.next().unwrap())
-        })
-    }
-    pub fn define_unary_plus<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
-        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
-              F: Fn(Args) -> eval::Result + 'static
-    {
-        self.define(name, Arity::Minimum(1), func)
-    }
-
-    pub fn define_binary<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
-        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
-              F: Fn(Value, Value) -> eval::Result + 'static
-    {
-        self.define(name, Arity::Exact(2), move |args: Args| {
-            let mut args = args.into_iter();
-            func(args.next().unwrap(), args.next().unwrap())
-        })
-    }
-    pub fn define_binary_plus<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
-        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
-              F: Fn(Args) -> eval::Result + 'static
-    {
-        self.define(name, Arity::Minimum(2), func)
-    }
-
-    pub fn define_ternary<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
-        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
-              F: Fn(Value, Value, Value) -> eval::Result + 'static
-    {
-        self.define(name, Arity::Exact(3), move |args: Args| {
-            let mut args = args.into_iter();
-            func(args.next().unwrap(),
-                 args.next().unwrap(),
-                 args.next().unwrap())
-        })
-    }
-    pub fn define_ternary_plus<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
-        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
-              F: Fn(Args) -> eval::Result + 'static
-    {
-        self.define(name, Arity::Minimum(3), func)
-    }
-}
-
-
-// Methods for defining the API functions which access the Context.
-impl<'c> Context<'c> {
+    /// Define a contextualized function with given arity, implemented using a Rust functgion.
+    ///
+    /// Contextualized functions receive, as their last argument, an (immutable) reference
+    /// to the Context they are invoked in. (This may be a child Context of the one they're
+    /// defined in). This allows them to indirectly invoke other functions via the Invoke trait.
+    ///
+    /// Returns a reference to the Context for easy chaining.
+    ///
     pub fn define_ctx<N: ?Sized, F>(&mut self, name: &'static N, arity: Arity, func: F) -> &mut Self
         where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
               F: Fn(Args, &Context) -> eval::Result + 'static
@@ -112,7 +60,30 @@ impl<'c> Context<'c> {
         self.set(name, Value::Function(function));
         self
     }
+}
 
+
+//
+// Methods for defining functions taking at least 0 arguments.
+//
+impl<'c> Context<'c> {
+    /// Define a regular function taking no arguments.
+    pub fn define_nullary<N:? Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
+        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
+              F: Fn() -> eval::Result + 'static
+    {
+        self.define(name, Arity::Exact(0), move |_| { func() })
+    }
+
+    /// Define a regular function taking zero or more arguments.
+    pub fn define_nullary_plus<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
+        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
+              F: Fn(Args) -> eval::Result + 'static
+    {
+        self.define(name, Arity::Minimum(0), func)
+    }
+
+    /// Define a contextualized function taking no arguments.
     pub fn define_nullary_ctx<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
         where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
               F: Fn(&Context) -> eval::Result + 'static
@@ -121,13 +92,41 @@ impl<'c> Context<'c> {
             func(&context)
         })
     }
+
+    /// Define a contextualized function taking zero or more arguments.
     pub fn define_nullary_plus_ctx<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
         where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
               F: Fn(Args, &Context) -> eval::Result + 'static
     {
         self.define_ctx(name, Arity::Minimum(0), func)
     }
+}
 
+
+//
+// Methods for defining functions taking at least 1 argument.
+//
+impl<'c> Context<'c> {
+    /// Define a regular function taking exactly one argument.
+    pub fn define_unary<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
+        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
+              F: Fn(Value) -> eval::Result + 'static
+    {
+        self.define(name, Arity::Exact(1), move |args: Args| {
+            let mut args = args.into_iter();
+            func(args.next().unwrap())
+        })
+    }
+
+    /// Define a regular function taking one or more arguments.
+    pub fn define_unary_plus<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
+        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
+              F: Fn(Args) -> eval::Result + 'static
+    {
+        self.define(name, Arity::Minimum(1), func)
+    }
+
+    /// Define a contextualized function taking exactly one argument.
     pub fn define_unary_ctx<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
         where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
               F: Fn(Value, &Context) -> eval::Result + 'static
@@ -137,13 +136,41 @@ impl<'c> Context<'c> {
             func(args.next().unwrap(), &context)
         })
     }
+
+    /// Define a contextualized function taking one or more arguments.
     pub fn define_unary_plus_ctx<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
         where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
               F: Fn(Args, &Context) -> eval::Result + 'static
     {
         self.define_ctx(name, Arity::Minimum(1), func)
     }
+}
 
+
+//
+// Methods for defining functions taking at least 2 arguments.
+//
+impl<'c> Context<'c> {
+    /// Define a regular function taking exactly two arguments.
+    pub fn define_binary<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
+        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
+              F: Fn(Value, Value) -> eval::Result + 'static
+    {
+        self.define(name, Arity::Exact(2), move |args: Args| {
+            let mut args = args.into_iter();
+            func(args.next().unwrap(), args.next().unwrap())
+        })
+    }
+
+    /// Define a regular function taking two or more arguments.
+    pub fn define_binary_plus<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
+        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
+              F: Fn(Args) -> eval::Result + 'static
+    {
+        self.define(name, Arity::Minimum(2), func)
+    }
+
+    /// Define a contextualized function taking exactly two arguments.
     pub fn define_binary_ctx<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
         where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
               F: Fn(Value, Value, &Context) -> eval::Result + 'static
@@ -154,13 +181,43 @@ impl<'c> Context<'c> {
                 &context)
         })
     }
+
+    /// Define a contextualized function taking two or more arguments.
     pub fn define_binary_plus_ctx<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
         where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
               F: Fn(Args, &Context) -> eval::Result + 'static
     {
         self.define_ctx(name, Arity::Minimum(2), func)
     }
+}
 
+
+//
+// Methods for defining functions taking at least 3 arguments.
+//
+impl<'c> Context<'c> {
+    /// Define a regular function taking exactly three arguments.
+    pub fn define_ternary<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
+        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
+              F: Fn(Value, Value, Value) -> eval::Result + 'static
+    {
+        self.define(name, Arity::Exact(3), move |args: Args| {
+            let mut args = args.into_iter();
+            func(args.next().unwrap(),
+                 args.next().unwrap(),
+                 args.next().unwrap())
+        })
+    }
+
+    /// Define a regular function taking three or more arguments.
+    pub fn define_ternary_plus<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
+        where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
+              F: Fn(Args) -> eval::Result + 'static
+    {
+        self.define(name, Arity::Minimum(3), func)
+    }
+
+    /// Define a contextualized function taking exactly three arguments.
     pub fn define_ternary_ctx<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
         where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
               F: Fn(Value, Value, Value, &Context) -> eval::Result + 'static
@@ -173,6 +230,8 @@ impl<'c> Context<'c> {
                  &context)
         })
     }
+
+    /// Define a contextualized function taking three or more arguments.
     pub fn define_ternary_plus_ctx<N: ?Sized, F>(&mut self, name: &'static N, func: F) -> &mut Self
         where Name: Borrow<N>, N: ToOwned<Owned=Name> + Hash + Eq + Display,
               F: Fn(Args, &Context) -> eval::Result + 'static
@@ -181,6 +240,8 @@ impl<'c> Context<'c> {
     }
 }
 
+
+// Utility functions
 
 /// Make sure a function got the correct number of arguments.
 /// Usage:
