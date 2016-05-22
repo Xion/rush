d48cbe1ddde29a7e30a321450e8e612fad1c9139
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-05-22 13:29:29 -0700

    Extract producing string output from Value to separate module

--- crates/librush/src/eval/model/value/mod.rs
+++ crates/librush/src/eval/model/value/mod.rs
@@ -8,14 +8,13 @@
 
 mod cmp;
 mod conv;
+mod output;
 mod types;
 
 
 use std::convert::From;
 use std::fmt;
 
-use conv::TryFrom;
-use conv::errors::GeneralError;
 use conv::misc::InvalidSentinel;
 use rustc_serialize::json::{Json, ToJson};
 
@@ -98,68 +97,6 @@ impl fmt::Debug for Value {
 
 
 // TODO(xion): extract the impl's below to submodules
-
-
-// Producing string output
-
-impl TryFrom<Value> for String {
-    type Err = GeneralError<&'static str>;
-
-    #[inline(always)]
-    fn try_from(src: Value) -> Result<Self, Self::Err> {
-        String::try_from(&src)
-    }
-}
-impl<'a> TryFrom<&'a Value> for String {
-    type Err = <String as TryFrom<Value>>::Err;
-
-    /// Try to convert a Value to string that can be emitted
-    /// as a final result of a computation.
-    fn try_from(src: &'a Value) -> Result<Self, Self::Err> {
-        match *src {
-            Value::Empty => Err(GeneralError::Unrepresentable(
-                "cannot serialize an empty value"
-            )),
-            Value::Symbol(ref t) => Ok(format!("{}", t)),
-            Value::Boolean(ref b) => Ok(format!("{}", b)),
-            Value::Integer(ref i) => Ok(format!("{}", i)),
-            Value::Float(ref f) => {
-                // always include decimal point and zero, even if the float
-                // is actually an integer
-                let mut res = f.to_string();
-                if !res.contains(".") {
-                    res.push_str(".0");
-                }
-                Ok(format!("{}", res))
-            },
-            Value::String(ref s) => Ok(s.clone()),
-            Value::Regex(..) => Err(GeneralError::Unrepresentable(
-                "cannot serialize a regex"
-            )),
-            Value::Array(ref a) => {
-                // for final display, an array is assumed to contain lines of output
-                Ok(format!("{}", a.iter()
-                    .map(|v| format!("{}", v)).collect::<Vec<String>>()
-                    .join("\n")))
-            },
-            Value::Object(..) => Ok(src.to_json().to_string()),
-            Value::Function(..) => Err(GeneralError::Unrepresentable(
-                "cannot serialize a function"
-            )),
-        }
-    }
-}
-
-impl fmt::Display for Value {
-    /// Format a Value for outputing it as a result of the computation.
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        String::try_from(self)
-            .map(|s| write!(fmt, "{}", s))
-            // TODO(xion): return an Err(fmt::Error) rather than panicking
-            // when formatting constructs actually react to it constructively
-            .expect(&format!("can't display a value of type `{}`", self.typename()))
-    }
-}
 
 
 // JSON conversions
---
+++ crates/librush/src/eval/model/value/output.rs
@@ -0,0 +1,73 @@
+//! Producing output from Value.
+//!
+//! This module defines how a Value is serialized as an output of the expression.
+//! See also the `json` module.
+
+use std::fmt;
+
+use conv::TryFrom;
+use conv::errors::GeneralError;
+use rustc_serialize::json::ToJson;
+
+use super::Value;
+
+
+impl TryFrom<Value> for String {
+    type Err = GeneralError<&'static str>;
+
+    #[inline(always)]
+    fn try_from(src: Value) -> Result<Self, Self::Err> {
+        String::try_from(&src)
+    }
+}
+impl<'a> TryFrom<&'a Value> for String {
+    type Err = <String as TryFrom<Value>>::Err;
+
+    /// Try to convert a Value to string that can be emitted
+    /// as a final result of a computation.
+    fn try_from(src: &'a Value) -> Result<Self, Self::Err> {
+        match *src {
+            Value::Empty => Err(GeneralError::Unrepresentable(
+                "cannot serialize an empty value"
+            )),
+            Value::Symbol(ref t) => Ok(format!("{}", t)),
+            Value::Boolean(ref b) => Ok(format!("{}", b)),
+            Value::Integer(ref i) => Ok(format!("{}", i)),
+            Value::Float(ref f) => {
+                // always include decimal point and zero, even if the float
+                // is actually an integer
+                let mut res = f.to_string();
+                if !res.contains(".") {
+                    res.push_str(".0");
+                }
+                Ok(format!("{}", res))
+            },
+            Value::String(ref s) => Ok(s.clone()),
+            Value::Regex(..) => Err(GeneralError::Unrepresentable(
+                "cannot serialize a regex"
+            )),
+            Value::Array(ref a) => {
+                // for final display, an array is assumed to contain lines of output
+                Ok(format!("{}", a.iter()
+                    .map(|v| format!("{}", v)).collect::<Vec<String>>()
+                    .join("\n")))
+            },
+            Value::Object(..) => Ok(src.to_json().to_string()),
+            Value::Function(..) => Err(GeneralError::Unrepresentable(
+                "cannot serialize a function"
+            )),
+        }
+    }
+}
+
+
+impl fmt::Display for Value {
+    /// Format a Value for outputing it as a result of the computation.
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        String::try_from(self)
+            .map(|s| write!(fmt, "{}", s))
+            // TODO(xion): return an Err(fmt::Error) rather than panicking
+            // when formatting constructs actually react to it constructively
+            .expect(&format!("can't display a value of type `{}`", self.typename()))
+    }
+}
