a3d0b52c6523688a35a11c1a31d65ac395988187
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-06-09 22:26:47 -0700

    Split tests for trailers into submodules

--- crates/librush/src/eval/api/base.rs
+++ crates/librush/src/eval/api/base.rs
@@ -15,9 +15,8 @@ pub fn len(value: Value) -> eval::Result {
     eval1!((value: &String) -> Integer { value.len() as IntegerRepr });
     eval1!((value: &Array) -> Integer { value.len() as IntegerRepr });
     eval1!((value: &Object) -> Integer { value.len() as IntegerRepr });
-    Err(Error::new(&format!(
-        "len() requires string/array/object, got {}", value.typename()
-    )))
+
+    mismatch!("len"; ("string") | ("array") | ("object") => (value))
 }
 
 /// Return an array of object's keys.
--- crates/librush/tests/tests/trailers.rs
+++ crates/librush/tests/tests/trailers.rs
@@ -1,80 +1,102 @@
 //! Tests for "trialers", i.e. final parts of atoms: subscripts & function calls.
 
-use util::*;
 
+mod subscript {
+    mod array {
+        use util::*;
 
-#[test]
-fn subscript_of_array_constant() {
-    assert_eq!("42", eval("[42][0]"));
-    assert_eq!("42", eval("[13, 42][1]"));
-    assert_eq!("42", eval("[[42]][0][0]"));
-    assert_eq!("c", eval("[a, b, c][-1]"));
-    assert_eval_error("[][0]");
-    assert_eval_error("[42][1]");
-    assert_eval_error("[42][-2]");
+        #[test]
+        fn constant() {
+            assert_eq!("42", eval("[42][0]"));
+            assert_eq!("42", eval("[13, 42][1]"));
+            assert_eq!("42", eval("[[42]][0][0]"));
+            assert_eq!("c", eval("[a, b, c][-1]"));
+            assert_eval_error("[][0]");
+            assert_eval_error("[42][1]");
+            assert_eval_error("[42][-2]");
+        }
+
+        #[test]
+        fn input() {
+            const INPUT: &'static [&'static str] = &["foo", "bar"];
+            assert_eq!("foo", apply_lines("_[0]", INPUT));
+            assert_eq!("bar", apply_lines("_[1]", INPUT));
+            assert_eq!("foo", apply_lines("[_][0][0]", INPUT));
+            assert_eq!("other", apply_lines("[_, [other]][1][0]", INPUT));
+            assert_apply_lines_error("_[42]", INPUT);
+        }
+    }
+
+    mod string {
+        use util::*;
+
+        #[test]
+        fn constant() {
+            assert_eq!("f", eval("foo[0]"));
+            assert_eq!("a", eval("\"bar\"[1]"));
+            assert_eval_error("\"\"[]");
+            assert_eval_error("baz[42]");
+        }
+
+        #[test]
+        fn input() {
+            const INPUT: &'static str = "hello";
+            assert_eq!("h", apply("_[0]", INPUT));
+            assert_eq!("l", apply("_[2]", INPUT));
+            assert_eq!("o", apply("_[-1]", INPUT));
+            assert_eq!("e", apply("_[-4]", INPUT));
+            assert_apply_error("_[42]", INPUT);
+            assert_apply_error("_[-42]", INPUT);
+        }
+    }
+
+    // TODO(xion): tests for subscript ranges
 }
 
-#[test]
-fn subscript_of_array_input() {
-    const INPUT: &'static [&'static str] = &["foo", "bar"];
-    assert_eq!("foo", apply_lines("_[0]", INPUT));
-    assert_eq!("bar", apply_lines("_[1]", INPUT));
-    assert_eq!("foo", apply_lines("[_][0][0]", INPUT));
-    assert_eq!("other", apply_lines("[_, [other]][1][0]", INPUT));
-    assert_apply_lines_error("_[42]", INPUT);
-}
+mod function_call {
+    mod one_arg {
+        use util::*;
 
-#[test]
-fn subscript_on_string_constant() {
-    assert_eq!("f", eval("foo[0]"));
-    assert_eq!("a", eval("\"bar\"[1]"));
-    assert_eval_error("\"\"[]");
-    assert_eval_error("baz[42]");
-}
+        #[test]
+        fn constant() {
+            assert_eq!("42", eval("abs(42)"));
+            assert_eq!("5", eval("len(hello)"));
+        }
 
-#[test]
-fn subscript_on_string_input() {
-    const INPUT: &'static str = "hello";
-    assert_eq!("h", apply("_[0]", INPUT));
-    assert_eq!("l", apply("_[2]", INPUT));
-    assert_eq!("o", apply("_[-1]", INPUT));
-    assert_eq!("e", apply("_[-4]", INPUT));
-    assert_apply_error("_[42]", INPUT);
-    assert_apply_error("_[-42]", INPUT);
-}
+        #[test]
+        fn input() {
+            assert_noop_apply("abs(_)", "42");
+            assert_eq!("5", apply("len(_)", "hello"));
+        }
+    }
 
-// TODO(xion): tests for subscript ranges
+    mod two_args {
+        use util::*;
 
-#[test]
-fn function_call_1arg_constant() {
-    assert_eq!("42", eval("abs(42)"));
-    assert_eq!("5", eval("len(hello)"));
-}
+        #[test]
+        fn constant() {
+            assert_eq!("he\n\no", eval("split(l, hello)"));
+        }
 
-#[test]
-fn function_call_1arg_input() {
-    assert_noop_apply("abs(_)", "42");
-    assert_eq!("5", apply("len(_)", "hello"));
-}
+        #[test]
+        fn input() {
+            assert_eq!("he\n\no", apply("split(l, _)", "hello"));
+        }
+    }
 
-#[test]
-fn function_call_2args_constant() {
-    assert_eq!("he\n\no", eval("split(l, hello)"));
-}
+    mod three_args {
+        use util::*;
 
-#[test]
-fn function_call_2args_input() {
-    assert_eq!("he\n\no", apply("split(l, _)", "hello"));
-}
+        #[test]
+        fn constant() {
+            assert_eq!("pot", eval("sub(i, o, pit)"));
+            assert_eq!("", eval("sub(a, \"\", aaa)"));
+        }
 
-#[test]
-fn function_call_3args_constant() {
-    assert_eq!("pot", eval("sub(i, o, pit)"));
-    assert_eq!("", eval("sub(a, \"\", aaa)"));
-}
-
-#[test]
-fn function_call_3args_input() {
-    assert_eq!("pot", apply("sub(i, o, _)", "pit"));
-    assert_eq!("", apply("sub(a, \"\", _)", "aaa"));
+        #[test]
+        fn input() {
+            assert_eq!("pot", apply("sub(i, o, _)", "pit"));
+            assert_eq!("", apply("sub(a, \"\", _)", "aaa"));
+        }
+    }
 }
