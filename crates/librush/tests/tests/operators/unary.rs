ef5222820eb2eea46efaef029b99dc1d728dc7b6
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-06-10 22:34:09 -0700

    Split the unary operator tests into submodules

--- crates/librush/tests/tests/operators/unary.rs
+++ crates/librush/tests/tests/operators/unary.rs
@@ -3,61 +3,73 @@
 use util::*;
 
 
-#[test]
-fn unary_plus_integer() {
-    assert_noop_apply("+_", "42");
-    assert_noop_apply("++_", "42");
-    assert_noop_apply("+++_", "42");
+mod plus {
+    use util::*;
+
+    #[test]
+    fn integer() {
+        assert_noop_apply("+_", "42");
+        assert_noop_apply("++_", "42");
+        assert_noop_apply("+++_", "42");
+    }
+
+    #[test]
+    fn float() {
+        assert_noop_apply("+_", "42.42");
+        assert_noop_apply("++_", "42.42");
+        assert_noop_apply("+++_", "42.42");
+    }
+
+    #[test]
+    fn string() {
+        assert_apply_error("+_", "foo");
+    }
+
+    #[test]
+    fn boolean() {
+        assert_apply_error("+_", "true");
+        assert_apply_error("+_", "false");
+    }
 }
 
-#[test]
-fn unary_plus_float() {
-    assert_noop_apply("+_", "42.42");
-    assert_noop_apply("++_", "42.42");
-    assert_noop_apply("+++_", "42.42");
+mod minus {
+    use util::*;
+
+    #[test]
+    fn integer() {
+        const INPUT: &'static str = "42";
+        let negated = format!("-{}", INPUT);
+        assert_eq!(negated, apply("-_", INPUT));
+        assert_eq!(INPUT, apply("--_", INPUT));
+        assert_eq!(negated, apply("---_", INPUT));
+    }
+
+    #[test]
+    fn float() {
+        const INPUT: &'static str = "42.42";
+        let negated = format!("-{}", INPUT);
+        assert_eq!(negated, apply("-_", INPUT));
+        assert_eq!(INPUT, apply("--_", INPUT));
+        assert_eq!(negated, apply("---_", INPUT));
+    }
 }
 
-#[test]
-fn unary_plus_string() {
-    assert_apply_error("+_", "foo");
-}
+mod bang {
+    use util::*;
 
-#[test]
-fn unary_plus_boolean() {
-    assert_apply_error("+_", "true");
-    assert_apply_error("+_", "false");
-}
+    #[test]
+    fn constant() {
+        assert_eq!("false", eval("!true"));
+        assert_eq!("true", eval("!!true"));
+        assert_eq!("false", eval("!!!true"));
+        assert_eq!("true", eval("!false"));
+        assert_eq!("false", eval("!!false"));
+        assert_eq!("true", eval("!!!false"));
+    }
 
-#[test]
-fn unary_minus_integer() {
-    const INPUT: &'static str = "42";
-    let negated = format!("-{}", INPUT);
-    assert_eq!(negated, apply("-_", INPUT));
-    assert_eq!(INPUT, apply("--_", INPUT));
-    assert_eq!(negated, apply("---_", INPUT));
-}
-
-#[test]
-fn unary_minus_float() {
-    const INPUT: &'static str = "42.42";
-    let negated = format!("-{}", INPUT);
-    assert_eq!(negated, apply("-_", INPUT));
-    assert_eq!(INPUT, apply("--_", INPUT));
-    assert_eq!(negated, apply("---_", INPUT));
-}
-
-#[test]
-fn unary_bang_constant() {
-    assert_eq!("false", eval("!true"));
-    assert_eq!("true", eval("!!true"));
-    assert_eq!("false", eval("!!!true"));
-    assert_eq!("true", eval("!false"));
-    assert_eq!("false", eval("!!false"));
-    assert_eq!("true", eval("!!!false"));
-}
-
-#[test]
-fn unary_bang_input() {
-    assert_eq!("false", apply("!_", "true"));
-    assert_eq!("true", apply("!_", "false"));
+    #[test]
+    fn input() {
+        assert_eq!("false", apply("!_", "true"));
+        assert_eq!("true", apply("!_", "false"));
+    }
 }
