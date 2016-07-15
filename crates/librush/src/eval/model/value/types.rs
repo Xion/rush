19be98617ca04640dc3a06572fbb4fd2eb120c61
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-07-14 23:36:19 -0700

    Add as_T() methods to Value

--- crates/librush/src/eval/model/value/types.rs
+++ crates/librush/src/eval/model/value/types.rs
@@ -24,13 +24,13 @@ pub type FunctionRepr = Function;
 
 
 /// Macro to implement methods on Value that deal with its various types.
-/// For a type X, those methods are is_X() & unwrap_X().
+/// For a type X, those methods are is_X(), unwrap_X(), and as_X().
 macro_rules! impl_value_type {
     // Unfortunately, due to Rust's strict macro hygiene requirements
     // and the general uselessness of concat_idents!, all the method names have to be given
     // as macro arguments.
     // More details: https://github.com/rust-lang/rust/issues/12249
-    ($variant:ident($t:ty) => ($is:ident, $unwrap:ident)) => (
+    ($variant:ident($t:ty) => ($is:ident, $unwrap:ident, $as_:ident)) => (
         impl Value {
             /// Check whether Value is of type $t.
             #[inline(always)]
@@ -47,20 +47,30 @@ macro_rules! impl_value_type {
                     _ => panic!(concat!(stringify!($unwrap), "() on {} value"), self.typename()),
                 }
             }
+
+            /// Returns a reference to the underlying $t.
+            /// Panics if the Value is not a $t.
+            #[inline]
+            pub fn $as_(&self) -> &$t {
+                match self {
+                    &Value::$variant(ref x) => x,
+                    _ => panic!(concat!(stringify!($as_), "() on {} value"), self.typename()),
+                }
+            }
         }
     );
 }
 
-impl_value_type!(Boolean(BooleanRepr)   => (is_bool,     unwrap_bool));
-impl_value_type!(Integer(IntegerRepr)   => (is_integer,  unwrap_integer));
-impl_value_type!(Integer(IntegerRepr)   => (is_int,      unwrap_int));  // alias
-impl_value_type!(Float(FloatRepr)       => (is_float,    unwrap_float));
-impl_value_type!(String(StringRepr)     => (is_string,   unwrap_string));
-impl_value_type!(String(StringRepr)     => (is_str,      unwrap_str));  // alias
-impl_value_type!(Regex(RegexRepr)       => (is_regex,    unwrap_regex));
-impl_value_type!(Array(ArrayRepr)       => (is_array,    unwrap_array));
-impl_value_type!(Object(ObjectRepr)     => (is_object,   unwrap_object));
-impl_value_type!(Function(FunctionRepr) => (is_function, unwrap_function));
+impl_value_type!(Boolean(BooleanRepr)   => (is_bool,     unwrap_bool,     as_bool));
+impl_value_type!(Integer(IntegerRepr)   => (is_integer,  unwrap_integer,  as_integer));
+impl_value_type!(Integer(IntegerRepr)   => (is_int,      unwrap_int,      as_int));  // alias
+impl_value_type!(Float(FloatRepr)       => (is_float,    unwrap_float,    as_float));
+impl_value_type!(String(StringRepr)     => (is_string,   unwrap_string,   as_string));
+impl_value_type!(String(StringRepr)     => (is_str,      unwrap_str,      as_str));  // alias
+impl_value_type!(Regex(RegexRepr)       => (is_regex,    unwrap_regex,    as_regex));
+impl_value_type!(Array(ArrayRepr)       => (is_array,    unwrap_array,    as_array));
+impl_value_type!(Object(ObjectRepr)     => (is_object,   unwrap_object,   as_object));
+impl_value_type!(Function(FunctionRepr) => (is_function, unwrap_function, as_function));
 
 
 /// Additional methods that deal with more than one type at once.
