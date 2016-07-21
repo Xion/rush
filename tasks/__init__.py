6f4196d8728daba4ee355e9b118c4c776e7311fd
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-07-20 17:11:30 -0700

    Convert print_grammar script to an Invoke task

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -4,20 +4,41 @@ Automation tasks, aided by the Invoke package.
 Most of them mimic the Rust's Cargo binary, but they do what makes sense for
 either the library crate, binary crate, or both.
 """
+from __future__ import print_function
+
+from glob import glob
 import logging
 import os
 import sys
 
-from invoke import Collection
+from invoke import Collection, task
 
 
 BIN = 'rush'
 LIB = 'librush'
 
 
-from tasks import build, clean, release, run, test
+@task(name="print-grammar")
+def print_grammar(ctx):
+    """Prints the language's grammar rules.
+    Uses comments in the syntax definition & parsing code.
+    """
+    syntax_package = './crates/%s/src/parse/syntax' % (LIB,)
+    for filename in glob('%s/*.rs' % syntax_package):
+        with open(filename) as f:
+            for line in f.readlines():
+                # TODO: support grammar rules spanning more than one line
+                if '::==' in line:
+                    line = line.lstrip('/').strip()
+                    print(line)
+
+
+# Task setup
 
 ns = Collection()
+ns.add_task(print_grammar)
+
+from tasks import build, clean, release, run, test
 ns.add_collection(build)
 ns.add_collection(clean)
 ns.add_collection(run)
