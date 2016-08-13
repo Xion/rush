e6b190373dba2cdf6ec2461fce232b134a0a9ad6
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2016-08-13 12:35:03 -0700

    len() to return string length in chars, not bytes

--- crates/librush/src/eval/api/base.rs
+++ crates/librush/src/eval/api/base.rs
@@ -40,7 +40,7 @@ pub fn flip(value: Value) -> eval::Result {
 
 /// Compute the length of given value (an array or a string).
 pub fn len(value: Value) -> eval::Result {
-    eval1!((value: &String) -> Integer { value.len() as IntegerRepr });
+    eval1!((value: &String) -> Integer { value.chars().count() as IntegerRepr });
     eval1!((value: &Array) -> Integer { value.len() as IntegerRepr });
     eval1!((value: &Object) -> Integer { value.len() as IntegerRepr });
     mismatch!("len"; ("string") | ("array") | ("object") => (value))
